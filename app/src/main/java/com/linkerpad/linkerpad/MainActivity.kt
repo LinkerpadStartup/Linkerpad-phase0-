c34162059b6cc670b5492b6256dc49534deb8f2c
Ali Hajiloo <haji.hd99@gmail.com>
2018-07-31 17:00:25 +0430

    tabs in MainActivity with Drawer added

--- app/src/main/java/com/linkerpad/linkerpad/MainActivity.kt
+++ app/src/main/java/com/linkerpad/linkerpad/MainActivity.kt
@@ -3,38 +3,67 @@ package com.linkerpad.linkerpad
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 import android.view.View
+import com.linkerpad.linkerpad.Adapters.RegLoginPagerAdapter
+import com.linkerpad.linkerpad.LoginRegisterFragments.LoginFragment
+import com.linkerpad.linkerpad.LoginRegisterFragments.RegisterFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        setupViewPager(mainPager)
+        tabs.setupWithViewPager(mainPager)
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = RegLoginPagerAdapter(supportFragmentManager)
+        adapter.addFragment(RegisterFragment(), "داشبورد")
+        adapter.addFragment(LoginFragment(), "پروژه ها")
+        viewPager.adapter = adapter
+    }
 
 
-        registerBtn.setOnClickListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.accountInfoMenu -> {
 
-            var intent = Intent(this@MainActivity , RegLoginHolder::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.faidin, R.anim.faidout)
+            }
 
-            registerBtn.visibility = View.INVISIBLE
-            loginBtn.visibility = View.INVISIBLE
+
         }
-        loginBtn.setOnClickListener {
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
-            var intent = Intent(this@MainActivity , RegLoginHolder::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.faidin, R.anim.faidout)
-            registerBtn.visibility = View.INVISIBLE
-            loginBtn.visibility = View.INVISIBLE
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+
+
         }
 
-
-        }
-
-
-
+    }
 
 }
