e7c100444305b371ac973589072516ab25f9bc85
Ali Hajiloo <haji.hd99@gmail.com>
2018-07-31 15:51:10 +0430

    some functions added to User View Model

---
+++ app/src/main/java/com/linkerpad/linkerpad/Data/UsersData.kt
@@ -0,0 +1,9 @@
+package com.linkerpad.linkerpad.Data
+
+/**
+ * Created by alihajiloo on 7/31/18.
+ */
+
+data class UsersData(var Name: String, var Email: String, var PassWord: String, var CompanyName: String, var PhoneNumber: String){
+
+}
--- app/src/main/java/com/linkerpad/linkerpad/Models/UserViewModel.kt
+++ app/src/main/java/com/linkerpad/linkerpad/Models/UserViewModel.kt
@@ -1,6 +1,20 @@
 package com.linkerpad.linkerpad.Models
 
+import com.linkerpad.linkerpad.Data.UsersData
+
+
 /**
  * Created by alihajiloo on 7/31/18.
  */
-data class UserViewModel(var Name: String, var Email: String, var PassWord: String, var CompanyName: String, var PhoneNumber: String )
+data class UserViewModel(var Name: String, var Email: String, var PassWord: String, var CompanyName: String, var PhoneNumber: String) {
+    companion object {
+        fun getUserViewModel(usersData: UsersData): UserViewModel {
+            return UserViewModel(usersData.Name, usersData.Email, usersData.PassWord, usersData.CompanyName, usersData.PhoneNumber)
+
+        }
+
+        fun getUsersData(userViewModel: UserViewModel): UsersData {
+            return UsersData(userViewModel.Name, userViewModel.Email, userViewModel.PassWord, userViewModel.CompanyName, userViewModel.PhoneNumber)
+        }
+    }
+}
