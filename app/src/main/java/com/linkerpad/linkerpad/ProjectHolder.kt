4e30aac54b13f275eacdb4f5b1c434bf1550d04c
Ali Hajiloo <haji.hd99@gmail.com>
2018-08-06 20:16:49 +0430

    some changes in ui

--- app/src/main/java/com/linkerpad/linkerpad/ProjectHolder.kt
+++ app/src/main/java/com/linkerpad/linkerpad/ProjectHolder.kt
@@ -1,6 +1,9 @@
 package com.linkerpad.linkerpad
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.FragmentManager
@@ -8,11 +11,19 @@ import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.TextView
 import com.linkerpad.linkerpad.Fragments.DataRequestFragment
 import com.linkerpad.linkerpad.Fragments.DocumentsFragment
 import com.linkerpad.linkerpad.Fragments.ReportsFragment
+import com.linkerpad.linkerpad.R.id.dataRequestNv
 import kotlinx.android.synthetic.main.project_holder_layout.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+import android.support.design.internal.BottomNavigationItemView
+import java.lang.reflect.AccessibleObject.setAccessible
+import java.lang.reflect.Array.setBoolean
+import android.support.design.internal.BottomNavigationMenuView
+
+
 
 class ProjectHolder : AppCompatActivity() {
 
@@ -24,6 +35,9 @@ class ProjectHolder : AppCompatActivity() {
         var fragmentManager: FragmentManager = supportFragmentManager
         var fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.projectsFrameLayout, ReportsFragment()).commit()
+
+
+     //   BottomNavigationViewHelper.removeShiftMode(navigation)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
@@ -52,6 +66,15 @@ class ProjectHolder : AppCompatActivity() {
                     fragmentTransaction.replace(R.id.projectsFrameLayout, ReportsFragment()).commit()
                     nvPosition = 1
                 }
+
+                if (moreCount == 1){
+                    var slideTopAnimaiton = AnimationUtils.loadAnimation(this@ProjectHolder, R.anim.abc_slide_out_bottom)
+                    moreNvll.startAnimation(slideTopAnimaiton)
+                    moreNvll.visibility = View.INVISIBLE
+
+                    moreCount = 0
+                }
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.documentsNv -> {
@@ -73,6 +96,14 @@ class ProjectHolder : AppCompatActivity() {
                     nvPosition = 2
                 }
 
+                if (moreCount == 1){
+                    var slideTopAnimaiton = AnimationUtils.loadAnimation(this@ProjectHolder, R.anim.abc_slide_out_bottom)
+                    moreNvll.startAnimation(slideTopAnimaiton)
+                    moreNvll.visibility = View.INVISIBLE
+
+                    moreCount = 0
+                }
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.dataRequestNv -> {
@@ -91,6 +122,14 @@ class ProjectHolder : AppCompatActivity() {
                     fragmentTransaction.hide(DataRequestFragment())
                     fragmentTransaction.replace(R.id.projectsFrameLayout, DataRequestFragment()).commit()*/
                     nvPosition = 3
+                }
+
+                if (moreCount == 1){
+                    var slideTopAnimaiton = AnimationUtils.loadAnimation(this@ProjectHolder, R.anim.abc_slide_out_bottom)
+                    moreNvll.startAnimation(slideTopAnimaiton)
+                    moreNvll.visibility = View.INVISIBLE
+
+                    moreCount = 0
                 }
                 return@OnNavigationItemSelectedListener true
             }
@@ -114,6 +153,32 @@ class ProjectHolder : AppCompatActivity() {
         }
         false
     }
+
+/*
+    internal object BottomNavigationViewHelper {
+
+        @SuppressLint("RestrictedApi")
+        fun removeShiftMode(view: BottomNavigationView) {
+            val menuView = view.getChildAt(0) as BottomNavigationMenuView
+            try {
+                val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+                shiftingMode.isAccessible = true
+                shiftingMode.setBoolean(menuView, false)
+                shiftingMode.isAccessible = false
+                for (i in 0 until menuView.childCount) {
+                    val item = menuView.getChildAt(i) as BottomNavigationItemView
+                    item.setShiftingMode(false)
+                    if (item.isSelected)
+                        item.setTextColor(ColorStateList(arrayOf<IntArray>(intArrayOf(Color.GRAY,Color.GRAY)), intArrayOf(Color.GRAY)))
+                    // set once again checked value, so view will be updated
+                    item.setChecked(item.itemData.isChecked)
+                }
+            } catch (e: NoSuchFieldException) {
+            } catch (e: IllegalAccessException) {
+            }
+
+        }
+    }*/
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
