a51da73df325aaf3566d06d4b3545a2ec42f24e9
Ali Hajiloo <haji.hd99@gmail.com>
2018-08-07 16:25:35 +0430

    Add Data Request Activity added and designed

---
+++ app/src/main/java/com/linkerpad/linkerpad/AddDataRequestActivity.kt
@@ -0,0 +1,75 @@
+package com.linkerpad.linkerpad
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.add_data_request_layout.*
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+
+class AddDataRequestActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_data_request_layout)
+
+
+        startDateCalender.setOnClickListener {
+            var intent = Intent(this@AddDataRequestActivity, ChooseDateActivity::class.java)
+            intent.putExtra("startOrEndDate", 1)
+            startActivityForResult(intent, 1)
+        }
+
+        endDateCalender.setOnClickListener {
+            var intent = Intent(this@AddDataRequestActivity, ChooseDateActivity::class.java)
+            intent.putExtra("startOrEndDate", 2)
+            startActivityForResult(intent, 1)
+        }
+
+
+        spinnerSetup()
+
+        //back click
+        addDataReqBackIcon.setOnClickListener { this@AddDataRequestActivity.finish() }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        Toast.makeText(this@AddDataRequestActivity, "Nothing Selected", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, id: Long) {
+        Toast.makeText(this@AddDataRequestActivity, "Selected position: #$position", Toast.LENGTH_SHORT).show()
+    }
+
+    fun spinnerSetup() {
+
+        val reqDataList = arrayOf("بایگانی", "تست", "تست۲")
+        val reqDataSpnAdapter = ArrayAdapter(this@AddDataRequestActivity, android.R.layout.simple_spinner_item, reqDataList)
+        reqDataSpnAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        reqDataSpn.adapter = reqDataSpnAdapter
+
+        reqDataSpn.onItemSelectedListener = this
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+
+        if (resultCode == 1)
+            createReqDataDateEdt.setText(data?.getStringExtra("date"))
+        else
+            finishReqDataDateEdt.setText(data?.getStringExtra("date"))
+
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+}
--- app/src/main/java/com/linkerpad/linkerpad/ProjectHolder.kt
+++ app/src/main/java/com/linkerpad/linkerpad/ProjectHolder.kt
@@ -1,28 +1,21 @@
 package com.linkerpad.linkerpad
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.TextView
 import com.linkerpad.linkerpad.Fragments.DataRequestFragment
 import com.linkerpad.linkerpad.Fragments.DocumentsFragment
 import com.linkerpad.linkerpad.Fragments.ReportsFragment
-import com.linkerpad.linkerpad.R.id.dataRequestNv
 import kotlinx.android.synthetic.main.project_holder_layout.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
-import android.support.design.internal.BottomNavigationItemView
-import java.lang.reflect.AccessibleObject.setAccessible
-import java.lang.reflect.Array.setBoolean
-import android.support.design.internal.BottomNavigationMenuView
-
+import android.widget.Toast
 
 
 class ProjectHolder : AppCompatActivity() {
@@ -37,8 +30,10 @@ class ProjectHolder : AppCompatActivity() {
         var fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.projectsFrameLayout, ReportsFragment()).commit()
 
-
-     //   BottomNavigationViewHelper.removeShiftMode(navigation)
+        addReqDataIcon.setOnClickListener {view ->
+            var intent = Intent(this@ProjectHolder , AddDataRequestActivity::class.java)
+            startActivity(intent)
+        }
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
@@ -167,7 +162,7 @@ class ProjectHolder : AppCompatActivity() {
 
                     moreCount = 0
                 }
-                
+
             }
 
         }
